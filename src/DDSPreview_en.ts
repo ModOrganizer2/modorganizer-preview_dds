4dddd545ed4d067208e824bece99b8771839e54d
Mikaël Capelle <capelle.mikael@gmail.com>
2024-07-21 16:49:04 +0200

    Update translations.

--- src/DDSPreview_en.ts
+++ src/DDSPreview_en.ts
@@ -4,67 +4,67 @@
   <context>
     <name>DDSFile</name>
     <message>
-      <location filename="DDS\DDSFile.py" line="37" />
+      <location filename="DDS\DDSFile.py" line="47" />
       <source>Magic number mismatch.</source>
       <translation type="unfinished" />
     </message>
     <message>
-      <location filename="DDS\DDSFile.py" line="91" />
+      <location filename="DDS\DDSFile.py" line="101" />
       <source>{0} (equivalent to {1})</source>
       <translation type="unfinished" />
     </message>
     <message>
-      <location filename="DDS\DDSFile.py" line="99" />
+      <location filename="DDS\DDSFile.py" line="109" />
       <source>Red bitmask {0}, Green bitmask {1}, Blue bitmask {2}</source>
       <translation type="unfinished" />
     </message>
     <message>
-      <location filename="DDS\DDSFile.py" line="105" />
+      <location filename="DDS\DDSFile.py" line="115" />
       <source>Luminance bitmask {0}</source>
       <translation type="unfinished" />
     </message>
     <message>
-      <location filename="DDS\DDSFile.py" line="110" />
+      <location filename="DDS\DDSFile.py" line="120" />
       <source>Alpha bitmask {0}</source>
       <translation type="unfinished" />
     </message>
     <message>
-      <location filename="DDS\DDSFile.py" line="112" />
+      <location filename="DDS\DDSFile.py" line="122" />
       <source>{0}×{1}</source>
       <translation type="unfinished" />
     </message>
     <message>
-      <location filename="DDS\DDSFile.py" line="114" />
+      <location filename="DDS\DDSFile.py" line="124" />
       <source>Cubemap</source>
       <translation type="unfinished" />
     </message>
     <message>
-      <location filename="DDS\DDSFile.py" line="114" />
+      <location filename="DDS\DDSFile.py" line="124" />
       <source>2D</source>
       <translation type="unfinished" />
     </message>
     <message>
-      <location filename="DDS\DDSFile.py" line="116" />
+      <location filename="DDS\DDSFile.py" line="126" />
       <source>Mipmapped</source>
       <translation type="unfinished" />
     </message>
     <message>
-      <location filename="DDS\DDSFile.py" line="116" />
+      <location filename="DDS\DDSFile.py" line="126" />
       <source>No mipmaps</source>
       <translation type="unfinished" />
     </message>
     <message>
-      <location filename="DDS\DDSFile.py" line="118" />
+      <location filename="DDS\DDSFile.py" line="128" />
       <source>{0}, {1} {2}, {3}</source>
       <translation type="unfinished" />
     </message>
     <message>
-      <location filename="DDS\DDSFile.py" line="140" />
+      <location filename="DDS\DDSFile.py" line="150" />
       <source>OpenGL driver incompatible with texture format.</source>
       <translation type="unfinished" />
     </message>
     <message>
-      <location filename="DDS\DDSFile.py" line="146" />
+      <location filename="DDS\DDSFile.py" line="156" />
       <source>Cubemap faces must be square</source>
       <translation type="unfinished" />
     </message>
@@ -72,47 +72,47 @@
   <context>
     <name>DDSPreview</name>
     <message>
-      <location filename="DDSPreview.py" line="436" />
+      <location filename="DDSPreview.py" line="435" />
       <source>Lets you preview DDS files by actually uploading them to the GPU.</source>
       <translation type="unfinished" />
     </message>
     <message>
-      <location filename="DDSPreview.py" line="442" />
+      <location filename="DDSPreview.py" line="441" />
       <source>If enabled, log OpenGL errors and debug messages. May decrease performance.</source>
       <translation type="unfinished" />
     </message>
     <message>
-      <location filename="DDSPreview.py" line="444" />
+      <location filename="DDSPreview.py" line="443" />
       <source>Red channel of background colour</source>
       <translation type="unfinished" />
     </message>
     <message>
-      <location filename="DDSPreview.py" line="445" />
+      <location filename="DDSPreview.py" line="444" />
       <source>Green channel of background colour</source>
       <translation type="unfinished" />
     </message>
     <message>
-      <location filename="DDSPreview.py" line="446" />
+      <location filename="DDSPreview.py" line="445" />
       <source>Blue channel of background colour</source>
       <translation type="unfinished" />
     </message>
     <message>
-      <location filename="DDSPreview.py" line="447" />
+      <location filename="DDSPreview.py" line="446" />
       <source>Alpha channel of background colour</source>
       <translation type="unfinished" />
     </message>
     <message>
-      <location filename="DDSPreview.py" line="448" />
+      <location filename="DDSPreview.py" line="447" />
       <source>The colour channels that are displayed.</source>
       <translation type="unfinished" />
     </message>
     <message>
-      <location filename="DDSPreview.py" line="484" />
+      <location filename="DDSPreview.py" line="491" />
       <source>Pick background colour</source>
       <translation type="unfinished" />
     </message>
     <message>
-      <location filename="DDSPreview.py" line="507" />
+      <location filename="DDSPreview.py" line="514" />
       <source>Select what colour channels are displayed.</source>
       <translation type="unfinished" />
     </message>
@@ -120,7 +120,7 @@
   <context>
     <name>DDSWidget</name>
     <message>
-      <location filename="DDSPreview.py" line="235" />
+      <location filename="DDSPreview.py" line="234" />
       <source>OpenGL debug message: {0}</source>
       <translation type="unfinished" />
     </message>
